83e7912b860b9648e74e0811afbacd3b29dd51ce
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2023-02-05 13:43:59 +0200

    white space fix

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -18,38 +18,38 @@ const Skills = () => {
 
     return (
         <div className='FullPage-section bg-black flex flex-col items-center pb-40' >
-            <h1 className='text-white text-center pt-10 text-3xl md:text-6xl font-bold'  data-aos="fade">SKILLS</h1>
+            <h1 className='text-white text-center pt-10 text-3xl md:text-6xl font-bold overflow-x-hidden'  data-aos="fade">SKILLS</h1>
             
-            <h1 className='text-white text-left text-xl md:text-4xl font-bold pt-10' data-aos="fade">Front-End</h1>
+            <h1 className='text-white text-left text-xl md:text-4xl font-bold pt-10 overflow-x-hidden' data-aos="fade">Front-End</h1>
             <div className="frontend-skills flex gap-10 py-5 flex-wrap items-center justify-center mx-4"  >
                 {frontend.map((skill) => (
-                    <div className='flex flex-col justify-center items-center gap-4 w-20' key={skill} data-aos="fade-right">
+                    <div className='flex flex-col justify-center items-center gap-4 w-20 overflow-x-hidden' key={skill} data-aos="fade-right">
                         <img src={`https://skillicons.dev/icons?i=${skill}`} alt="" className='w-20' />
                         <p className='text-white text-lg'>{skill}</p>
                     </div>
                 ))}
             </div>
 
-            <h1 className='text-white text-left text-xl md:text-4xl font-bold pt-10' data-aos="fade">Back-End</h1>
+            <h1 className='text-white text-left text-xl md:text-4xl font-bold pt-10 overflow-x-hidden' data-aos="fade">Back-End</h1>
             <div className="frontend-skills flex gap-10 py-5 flex-wrap items-center justify-center mx-4" >
                 {backend.map((skill) => (
-                    <div className='flex flex-col justify-center items-center gap-4 w-20' key={skill} data-aos="fade-right">
+                    <div className='flex flex-col justify-center items-center gap-4 w-20 overflow-x-hidden' key={skill} data-aos="fade-right">
                         <img src={`https://skillicons.dev/icons?i=${skill}`} alt="" className='w-20' />
                         <p className='text-white text-lg'>{skill}</p>
                     </div>
                 ))}
             </div>
             
-            <h1 className='text-white text-left text-xl md:text-4xl font-bold pt-10' data-aos="fade">Miscellaneous</h1>
+            <h1 className='text-white text-left text-xl md:text-4xl font-bold pt-10 overflow-x-hidden' data-aos="fade">Miscellaneous</h1>
             <div className="frontend-skills flex gap-10 py-5 flex-wrap items-center justify-center mx-4"  >
                 {Miscellaneous.map((skill) => (
-                    <div className='flex flex-col justify-center items-center gap-4 w-20' key={skill} data-aos="fade-right">
+                    <div className='flex flex-col justify-center items-center gap-4 w-20 overflow-x-hidden' key={skill} data-aos="fade-right">
                         <img src={`https://skillicons.dev/icons?i=${skill}`} alt="" className='w-20'/>
                         <p className='text-white text-lg'>{skill}</p>
                     </div>
                 ))}
-                <div className='flex flex-col justify-center items-center gap-4 ' data-aos="fade-right" >
-                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" className='w-20' alt="npm logo"  />
+                <div className='flex flex-col justify-center items-center gap-4 overflow-x-hidden' data-aos="fade-right" >
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" className='w-20 ' alt="npm logo"  />
                     <p className='text-white text-lg' >NPM</p>
                 </div>
             </div>
