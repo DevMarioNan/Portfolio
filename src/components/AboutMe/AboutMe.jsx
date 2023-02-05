cbccd62b3fbc7bd6322f6873f6af91d8d1cbd1e9
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2023-02-05 15:04:23 +0200

    changed frontend engineer to frontend software engineer

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -56,7 +56,7 @@ const AboutMe = () => {
                         className="underline font-bold "
                         onMouseEnter={(e) => handleHover(e,true)}
                         onMouseLeave={(e) => handleLeave(e)}
-                    >Mario Nan</span> , I'm a Front-End Engineer
+                    >Mario Nan</span> , I'm a Front-End Software Engineer
                 </h1>
                 
             </div>
