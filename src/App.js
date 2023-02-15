4d661520be57a7129576a2741fd6433fbaf445e1
DevMarioNan <80198355+DevMarioNan@users.noreply.github.com>
2023-02-16 01:10:29 +0200

    npm i '@vercel/analytics

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import FullPage from './components/FullpageScroll/FullPage';
 import "aos/dist/aos.css";
 import AOS from "aos";
 import { useEffect } from 'react';
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   useEffect(() => {
     AOS.init();
     AOS.refresh();
   }, []);
-
+  
+  
   return (
+    <>
     <FullPage />
+    <Analytics />
+    </>
   );
+
 }
 
 export default App;
